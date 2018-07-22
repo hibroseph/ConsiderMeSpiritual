cc5f068c17471f9f2dd772ce617d046761ace6c4
Joe <iwillridgley@gmail.com>
2018-07-22 11:34:04 -0600

    added increment string method to increment last char of string by 1

--- app/src/main/java/joellc/considermespiritual/MainActivity.java
+++ app/src/main/java/joellc/considermespiritual/MainActivity.java
@@ -37,6 +37,11 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager viewPager;
     private SharedPreferences prefs;
 
+    // For debugging
+    private String TAG;
+
+    private FirebaseDatabase firebaseDatabase;
+
     private int[] tabIcons = {
             R.drawable.chat_icon_24,
             R.drawable.thumb_up_24,
@@ -48,6 +53,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Set the TAG for logging
+        TAG = this.getLocalClassName();
+
+        // Get the preference Manager.
         prefs = PreferenceManager.getDefaultSharedPreferences(this);
 
         setSupportActionBar(findViewById(R.id.toolbar));
@@ -61,156 +70,24 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(viewPager);
 
         setupTabIcons();
-        String TAG = "MainActivity";
 
         // Get the shared preferences and make sure they can edit
 
         // CURRENT DEBUGGING SECTION
-        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
 
-        // Put this just to see where we can start
-        SharedPreferences.Editor editor= prefs.edit();
-        editor.putString("Last_Downloaded", "-LGOEw6mcW-R4N1wK5us").apply();
+        addToSharedPreferences("LAST_DOWNLOADED", "-LGOEw6muos-OonrZOnx");
 
-        String startAtDownload = prefs.getString("Last_Downloaded", "");
-
-        if (startAtDownload != null) {
-            Query DownloadNewQuotesQuery = db.getReference("Quotes").orderByChild("id").startAt(startAtDownload);
-
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    DownloadNewQuotesQuery.addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                            String lastDownloaded = null;
-                            for (DataSnapshot snappy : dataSnapshot.getChildren()) {
-
-                                Log.d(TAG, "An Object");
-                                SpiritualToken sp = snappy.getValue(SpiritualToken.class);
-                                Log.d(TAG, sp.getID());
-                                Log.d(TAG, sp.getQuote());
-                                lastDownloaded = sp.getID();
-                            }
-
-                            Log.d(TAG, "The last downloaded id was: " + lastDownloaded);
-
-                            // See if anything was downloaded
-                            if (lastDownloaded != null) {
-                                SharedPreferences.Editor editor = prefs.edit();
-                                editor.putString("Last_Downloaded", lastDownloaded).apply();
-                            }
-                        }
-
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                        }
-                    });
-                }
-            }).start();
-        } else {
-            Thread downloadQuotesThread = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    FirebaseDatabase database = FirebaseDatabase.getInstance();
-
-                    // Get a reference to the Firebase where the quotes are stored.
-                    DatabaseReference databaseRef = database.getReference("Quotes");
-
-                    databaseRef.addValueEventListener(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(DataSnapshot dataSnapshot) {
-                            // This is where the data should get saved into the database.
-
-                            Log.d(TAG, "The path quotes has: " + dataSnapshot.getChildrenCount() +
-                                    " children. Let's loop through them all to download them");
+        String startAtDownload = prefs.getString("LAST_DOWNLOADED", null);
 
 
-                            String lastDownloaded = null;
-                            for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+        findNumOfQuotesToDownload("");
 
-                                Log.d(TAG, "A KEY: " + postSnapshot.getKey());
-
-                                // Get child at specific address
-                                SpiritualToken st = dataSnapshot.child(postSnapshot.getKey()).getValue(SpiritualToken.class);
-
-                                lastDownloaded = st.getID();
-                                // Check to see if you have a scripture
-                                if (st.getAuthor().matches(".*\\d.*")) {
-                                    Log.d(TAG, "THIS CONTAINS A NUMBER. SCRIPTURE");
-                                    Log.d(TAG, "See if I am wrong, is it a scripture? " + st.getAuthor());
-                                    st.setScripture(TRUE);
-                                }
-
-                                Thread loadTokensIntoDatabase = new Thread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        Database.getDatabase(getApplicationContext()).spiritualTokenDao().addSpiritualToken(st);
-                                    }
-                                });
-
-                                loadTokensIntoDatabase.start();
-                            }
-
-                            // By time the code runs here, it should be done.
-                            Log.d(TAG, "I think the firebase has finished loading data");
-
-                            // Store in the sharedPreferences the last downloaded item
-                            SharedPreferences.Editor editor = prefs.edit();
-                            editor.putString("Last_Downloaded", lastDownloaded).apply();
-
-                            // Let's tell the fragment that we are done downloading
-//                        SearchForQuoteFragment searchForQuoteFragment1 = (SearchForQuoteFragment) getSupportFragmentManager().findFragmentByTag("frag1");
-//                        searchForQuoteFragment1.onSuccess();
-
-                        }
-
-                        @Override
-                        public void onCancelled(DatabaseError databaseError) {
-                            // This shouldn't run but if it does, bad things happened that need to be
-                            // researched
-
-                            Log.d(TAG, "BAD STUFF HAPPENED " + databaseError.getDetails());
-                        }
-
-                    });
-                }
-            });
-
-            // Start the thread
-            Log.d(TAG, "Starting the thread");
-            downloadQuotesThread.start();
-
-
-//        FloatingActionButton fab = findViewById(R.id.floatingActionButtonScrollToTop);
-
-//        fab.hide();
-
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Log.d(TAG, "You clicked the FAB");
-//
-//                // Top?
-//                nsv.scrollTo(0,0);
-//            }
-//        });
-//
-//        nsv.setOnScrollChangeListener(new NestedScrollView.OnScrollChangeListener() {
-//            @Override
-//            public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
-//                Log.d(TAG, "You are scrolling");
-//                Log.d(TAG, "You are at position " + scrollY);
-//                if (scrollY > 639 && fab.getVisibility() != View.VISIBLE) {
-//                    fab.show();
-//                } else if (scrollY <639 && fab.getVisibility() == View.VISIBLE) {
-//                    fab.hide();
-//                }
-//            }
-//        });
-        }
+//        if (startAtDownload != null) {
+//            findNumOfQuotesToDownload(startAtDownload);
+//        } else {
+//            findNumOfQuotesToDownload("");
+//        }
 
         new Thread(new Runnable() {
             @Override
@@ -220,6 +97,67 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "The size of the table is: " + size);
             }
         }).start();
+    }
+
+    // Increments a push id that was saved from Firebase by 1
+    public String incrementPushIdBy1(String Id) {
+        // TODO: Handle incrementing where last letter is z
+
+        // Get the position of the last char
+        int posOfLastChar = (Id.length() - 1);
+
+        // Get that char
+        char temp = Id.charAt(posOfLastChar);
+
+        // Get a charArray to be able to change the last value
+        char charArray[] = Id.toCharArray();
+
+        // Increment the last char by 1 and add it to the char array
+        charArray[posOfLastChar] = (char)(temp + 1);
+
+        // Return a new string
+        return new String(charArray);
+    }
+
+    // This function returns the number of quotes that exist to download according to the last id
+    // stored that was downloaded
+    private void findNumOfQuotesToDownload(String lastDownloadedString) {
+
+        Query DownloadNewQuotesQuery = firebaseDatabase.getReference("Quotes").orderByChild("id").startAt(lastDownloadedString);
+
+        // Create a new thread to access Firebase
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                DownloadNewQuotesQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        long numOfQuotes = dataSnapshot.getChildrenCount();
+
+                        Log.d(TAG, "You have " + numOfQuotes + " to download");
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        //TODO: Handle error
+                    }
+                });
+            }
+        }).start();
+    }
+
+
+    /**
+     * Adds the parameters passed to shared preferences. This was made in attempt to size down and
+     * simplify the onCreate function
+     * @param key The key to be placed in the sharedPreferences
+     * @param value The value to be placed at the key in the sharedPreferences
+     */
+    private void addToSharedPreferences(String key, String value) {
+        SharedPreferences.Editor editor = prefs.edit();
+
+        editor.putString(key, value).apply();
 
     }
 
--- app/src/test/java/joellc/considermespiritual/ExampleUnitTest.java
+++ app/src/test/java/joellc/considermespiritual/ExampleUnitTest.java
@@ -18,5 +18,34 @@ public class ExampleUnitTest {
         assertEquals(4, 2 + 2);
     }
 
+    // Tests the function incrementPushIdBy1 to make sure the last character increases by 1
+    @Test
+    public void incrementCharTest() throws Exception {
+        MainActivity activity = new MainActivity();
+
+        // Case 1 Simple id increment
+        String string = "-LGOEw6muos-OonrZOnx";
+        String incrementedString = activity.incrementPushIdBy1(string);
+        assertEquals("-LGOEw6muos-OonrZOny", incrementedString);
+
+        // Case 2 Simple name increment
+        string = "Joseph";
+        incrementedString = activity.incrementPushIdBy1(string);
+        assertNotEquals("Joseph", incrementedString);
+        assertEquals("Josepi", incrementedString);
+
+        // Case 3 Simple number increment
+        string = "1";
+        incrementedString = activity.incrementPushIdBy1(string);
+        assertEquals("2", incrementedString);
+
+        // TODO: Add cases to handle weird incrementing like this
+        // Case 4 Increment needs to change 2 chars
+//        string = "LGOEw6muos-OonrZOnz";
+//        incrementedString = activity.incrementPushIdBy1(string);
+//        assertEquals("LGOEw6muos-OonrZOnz", incrementedString);
+
+    }
+
 }
 
